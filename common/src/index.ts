852b0a7f47796e51bfec702aa370783ab25fff9b
Moustafa Aly <m.raeia@hotmail.com>
2022-01-09 19:59:43 -0500

    updated version

--- common/src/index.ts
+++ common/src/index.ts
@@ -12,5 +12,6 @@ export * from "./middlewares/validate-req";
 
 export * from "./events/listener";
 export * from "./events/product-created-event";
+export * from "./events/product-updated-event";
 export * from "./events/publisher";
 export * from "./events/subjects";
